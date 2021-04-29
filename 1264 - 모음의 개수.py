2a3f86a4d74a2d134522388bbdbc4d37bec4c544
skdbsxir <skdbsxir@naver.com>
2021-04-29 22:06:55 +0900

    커밋 잘못취소함ㅠ... 로직생각/구현 적어도 1시간은 잡힌듯.

---
+++ 1264 - 모음의 개수.py
@@ -0,0 +1,15 @@
+myText = [] # 입력받은 텍스트를 저장할 리스트 선언.
+myVowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'] # 모음 리스트.
+
+while True :
+    vowelCount = 0
+    myText = input()
+
+    # '#'입력하면 끝.
+    if myText == '#' :
+        break
+
+    for i in myText:
+        if i in myVowel :
+            vowelCount += 1
+    print(vowelCount) 
